291bbbffc9396fc1e3714e745f351971955fd7fc
Pavel Stepanov <pstepanov.spb@gmail.com>
2022-10-29 08:43:51 +0300

    initial version

---
+++ TorchClassifier/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using TorchClassifier;
+using TorchClassifier.NamesDatabase;
+
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Names classifier");
+
+
+var classifier = new NamesClassifier();
+classifier.Run();
